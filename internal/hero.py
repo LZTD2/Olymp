5020b0338bb128991d35e1cf23aaa8b714e74d3f
Danil <danil@gmail.com>
2025-02-25 20:38:24 +0300

    Добавил класс Hero

---
+++ internal/hero.py
@@ -0,0 +1,8 @@
+class hero():
+    def __init__(self     ):
+        self.x  =      1
+        self.y  =    1
+    def get_x(self):
+        return self.x
+    def getY(self):
+        return self.y
